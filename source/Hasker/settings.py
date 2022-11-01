fec40b21b7054384308a560d0de8c68d3a1a9bfe
disugiske <disugiske@yandex.ru>
2022-11-02 01:16:55 +0300

    Add Dokerfile

--- source/Hasker/settings.py
+++ source/Hasker/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'disugiske@gmail.com '
-EMAIL_HOST_PASSWORD = 'szqmfiedvsnhlhwb'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
